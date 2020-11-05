b671ae4e22ef2e620b6f418fd2a8e82d3c5df926
kazuki.mera <mera@brewus.co.jp>
2020-11-05 16:13:37 +0900

    build

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
+              destinationDir: 'static',
               ignoreFileExtensions: [],
             }
           },
